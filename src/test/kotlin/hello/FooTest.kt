24a9662ea01f0237b327ecc9960a908b60445c4b
sumimaru <sumiyoshi102@gmail.com>
2018-12-26 10:44:14 +0900

    Update FooTest.kt

--- src/test/kotlin/hello/FooTest.kt
+++ src/test/kotlin/hello/FooTest.kt
@@ -15,5 +15,26 @@ class FooTest {
         val foo = Foo()
         assertEquals("test2", foo.method2())
     }
+    
+    @Test
+    fun testScopeFunction() {
+
+        val name = "some name"
+
+        assertEquals("some name".let { it.toUpperCase() }, "SOME NAME")
+        assertEquals("some name".run { toUpperCase() }, "SOME NAME")
+        assertEquals("some name".apply { toUpperCase() }, "some name")
+        assertEquals("some name".also { it.toUpperCase() }, "some name")
+
+        class Data(var name: String)
+        val _data1 = Data(name)
+        val _data2 = Data(name)
+
+        val data1 = _data1.apply { this.name = name.toUpperCase() }
+        val data2 = _data2.also { it.name = it.name.toUpperCase() }
+
+        assertEquals(data1.name, "SOME NAME")
+        assertEquals(data2.name, "SOME NAME")
+    }
 
 }
